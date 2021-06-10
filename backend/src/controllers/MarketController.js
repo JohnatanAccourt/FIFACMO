34ecf074fb16e3b20cd70fd1b7c0a6cf1cbaa97a
JohnatanAccourt <johnatan1999accourt@gmail.com>
2021-06-10 19:21:42 -0300

    reborn

--- backend/src/controllers/MarketController.js
+++ backend/src/controllers/MarketController.js
@@ -5,43 +5,64 @@ module.exports = {
     async index(req, res){
         const page = req.query.page
 
-        const playersSearch = await players.findAll( //findAndCountAll
-            {
-                raw : true,
-                limit: 5,
-                offset: ((page - 1) * 5),
-                subQuery: false,
-                order: [[ 'overall','DESC' ]],
-                where: { team_id: null },
-                attributes:{
-                    exclude:[
-                        'dob', 
-                        'potential', 
-                        'wage_eur', 
-                        'international_reputation', 
-                        'work_rate', 'body_type', 
-                        'release_clause_eur', 
-                        'team_position', 
-                        'team_jersey_number', 
-                        'loaned_from', 
-                        'joined',
-                        'contract_valid_until',
-                        'nation_position',
-                        'nation_jersey_number',
-                        'favorites_id',
-                        'cart_id'
-                    ]
-                }
-            }
-        )
+        // const playersSearch = await players.findAll( //findAndCountAll
+        //     {
+        //         raw : true,
+        //         limit: 5,
+        //         offset: ((page - 1) * 5),
+        //         subQuery: false,
+        //         order: [[ 'overall','DESC' ]],
+        //         where: { team_id: null },
+        //         attributes:{
+        //             exclude:[
+        //                 'dob', 
+        //                 'potential', 
+        //                 'wage_eur', 
+        //                 'international_reputation', 
+        //                 'work_rate', 'body_type', 
+        //                 'release_clause_eur', 
+        //                 'team_position', 
+        //                 'team_jersey_number', 
+        //                 'loaned_from', 
+        //                 'joined',
+        //                 'contract_valid_until',
+        //                 'nation_position',
+        //                 'nation_jersey_number',
+        //                 'favorites_id',
+        //                 'cart_id'
+        //             ]
+        //         }
+        //     }
+        // )
 
         // using this is much more slow although easy to front-end catch data
 
-        const count = await players.findAndCountAll({
-            raw : true,
-        })
+        // const count = await players.findAndCountAll({
+        //     raw : true,
+        // })
 
-        res.header('X-Total-Count', count.count)
+        // res.header('X-Total-Count', count.count)
+
+        const playersSearch = await players.findAll( //findAndCountAll
+            {
+                raw : true,
+                limit: 10,
+                offset: ((page - 1) * 5),
+                subQuery: false,
+                order: [[ 'overall', 'DESC' ]],
+                where: { team_id: null, },
+                // nest : true,
+                attributes:[
+                    'sofifa_id',
+                    'short_name',
+                    'age',
+                    'weight_kg',
+                    'nationality',
+                    'overall',
+                    'value_eur',
+                ],
+            }
+        )
 
         return res.json(playersSearch)
     },
